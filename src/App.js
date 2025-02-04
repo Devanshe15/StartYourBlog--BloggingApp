59f87b2df1bc15b1db62900104539300cdc3d16e
Devanshe sharma <devanshesharma6@gmail.com>
2025-02-04 21:03:43 +0530

    intial commit
    Signed-off-by: Devanshe sharma <devanshesharma6@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Signin />
+        <Signup />
+      </header>
+    </div>
+  );
+}
+
+export default App;
